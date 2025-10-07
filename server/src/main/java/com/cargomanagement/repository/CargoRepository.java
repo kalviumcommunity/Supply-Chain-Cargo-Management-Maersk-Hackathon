95fac8b240b1328322a3b141101768d3c30b4fc4
rahulrr-coder <rahulrr5002@protonmail.com>
2025-10-07 15:40:39 +0530

    feat: enhance repository interfaces with additional query methods

--- server/src/main/java/com/cargomanagement/repository/CargoRepository.java
+++ server/src/main/java/com/cargomanagement/repository/CargoRepository.java
@@ -4,5 +4,10 @@ import com.cargomanagement.models.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CargoRepository extends JpaRepository<Cargo, Long> {}
+public interface CargoRepository extends JpaRepository<Cargo, Long> {
+    List<Cargo> findByShipmentShipmentId(Long shipmentId);
+    List<Cargo> findByType(String type);
+}
--- server/src/main/java/com/cargomanagement/repository/RouteRepository.java
+++ server/src/main/java/com/cargomanagement/repository/RouteRepository.java
@@ -4,5 +4,11 @@ import com.cargomanagement.models.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RouteRepository extends JpaRepository<Route, Long> {}
+public interface RouteRepository extends JpaRepository<Route, Long> {
+    List<Route> findByStatus(String status);
+    List<Route> findByOriginPortAndDestinationPort(String originPort, String destinationPort);
+    List<Route> findByTransportationMode(String transportationMode);
+}
